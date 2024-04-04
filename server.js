38bfca1adf8fefcc5a446502733c05a441a363ac
alenzo <alenzo.amico@gmail.com>
2024-04-04 16:21:53 +0200

    add folder css and js

--- server.js
+++ server.js
@@ -7,27 +7,27 @@ app.use(express.static(path.join(__dirname)));
 
 // Route pour servir l'index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static/index.html'));
+    res.sendFile(path.join(__dirname, 'template/index.html'));
 });
 
 app.get('/tic_tac_toe', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static/tic_tac_toe.html'));
+    res.sendFile(path.join(__dirname, 'template/tic_tac_toe.html'));
 });
 
 app.get('/snake', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static/snake.html'));
+    res.sendFile(path.join(__dirname, 'template/snake.html'));
 });
 
 app.get('/keyboard_cps', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static/keyboard_cps.html'));
+    res.sendFile(path.join(__dirname, 'template/keyboard_cps.html'));
 });
 
 app.get('/memory_test', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static/memory_test.html'));
+    res.sendFile(path.join(__dirname, 'template/memory_test.html'));
 });
 
 app.get('/visual_sort', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static/visual_sort.html'));
+    res.sendFile(path.join(__dirname, 'template/visual_sort.html'));
 });
 
 // Démarrer le serveur
