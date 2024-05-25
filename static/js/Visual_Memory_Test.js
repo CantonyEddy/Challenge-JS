e05022ae3fac98b3e0303a85884d28b7412157e9
unknown <ALEXANDRE@LAPTOP-2945L9RP>
2024-05-25 19:51:13 +0200

    Visual_Memory_Test

--- static/js/Visual_Memory_Test.js
+++ static/js/Visual_Memory_Test.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
     const gameBoard = document.getElementById('game-board');
     const startButton = document.getElementById('start-game');
     let squares = [];
     let activeSquares = [];
 
     function createSquares() {
+        console.log("Creating squares...");
         const boardRows = document.querySelectorAll('.board-row');
         boardRows.forEach(row => {
             const cells = row.querySelectorAll('.board-cell');
@@ -15,12 +18,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 });
             });
         });
+        console.log("Squares created:", squares.length);
     }
 
     function randomlyActivateSquares(numActive) {
+        console.log("Randomly activating squares...");
         const shuffledSquares = squares.slice().sort(() => 0.5 - Math.random());
         activeSquares = shuffledSquares.slice(0, numActive);
         activeSquares.forEach(square => square.classList.add('active'));
+        console.log("Active squares:", activeSquares.length);
     }
 
     function toggleActive(square) {
@@ -32,17 +38,20 @@ document.addEventListener("DOMContentLoaded", function() {
             square.classList.add('active');
             activeSquares.push(square);
         }
+        console.log("Toggled square, active squares count:", activeSquares.length);
     }
 
     function clearBoard() {
+        console.log("Clearing board...");
         activeSquares.forEach(square => square.classList.remove('active'));
         activeSquares = [];
+        console.log("Board cleared");
     }
 
-    // Écouteur d'événement pour le bouton de démarrage du jeu
     startButton.addEventListener('click', () => {
+        console.log("Start button clicked");
         createSquares();
-        randomlyActivateSquares(3); // Changez le nombre de carrés actifs au début ici
-        setTimeout(clearBoard, 5000); // Désactive tous les carrés après 5 secondes
+        randomlyActivateSquares(3);
+        setTimeout(clearBoard, 5000);
     });
 });
